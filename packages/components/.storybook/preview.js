a3df82d98154962f76e6595fe178100e8a967693
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-09-04 21:59:34 +0100

    docs: upgrade storybook

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import '@transferwise/neptune-css/dist/css/neptune.css';
 import 'currency-flags/dist/currency-flags.min.css';
 import '@transferwise/icons/dist/icons.min.css';
 
-const style = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '80px',
-  minHeight: '100vh',
-  width: '100vw',
+export const parameters = {
+  layout: 'padded',
+  panelPosition: 'right',
 };
 
 export const decorators = [
   (Story) => (
-    <div style={style}>
-      <div style={{ width: '100%' }}>
-        <Story />
-      </div>
-    </div>
+    <StrictMode>
+      <Story />
+    </StrictMode>
   ),
 ];
