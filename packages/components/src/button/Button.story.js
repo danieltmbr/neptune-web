fc317ce8ad738b62aebaece842ae0874a6028717
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-09-04 22:13:10 +0100

    chore: example button

--- packages/components/src/button/Button.story.js
+++ packages/components/src/button/Button.story.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import Button from './Button';
-import { action } from '@storybook/addon-actions';
-import { boolean, select, text } from '@storybook/addon-knobs';
 
 export default {
   component: Button,
   title: 'Button',
+  argTypes: { onClick: { action: 'clicked' } },
 };
 
-export const basic = () => {
-  const size = select('size', Object.values(Button.Size), Button.Size.SMALL);
-  const type = select('type', Object.values(Button.Type), Button.Type.PRIMARY);
-  const htmlType = select('htmlType', ['button', 'submit', 'reset'], 'button');
-  const disabled = boolean('disabled', false);
-  const loading = boolean('loading', false);
-  const block = boolean('block', false);
-  const buttonText = text('buttonText', 'Button text');
-  return (
-    <Button
-      size={size}
-      type={type}
-      disabled={disabled}
-      loading={loading}
-      block={block}
-      htmlType={htmlType}
-      onClick={action('Button Clicked')}
-    >
-      {buttonText}
-    </Button>
-  );
-};
+export const Basic = (args) => <Button {...args}>A button</Button>;
