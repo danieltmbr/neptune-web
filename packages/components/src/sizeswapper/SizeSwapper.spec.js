5fe58eaba344e87ee5eb0412e52c7f4740a69095
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-10-16 09:05:42 +0100

    test: add intitial test

--- packages/components/src/sizeswapper/SizeSwapper.spec.js
+++ packages/components/src/sizeswapper/SizeSwapper.spec.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import SizeSwapper from './';
+import { render, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import ElementQueries from 'css-element-queries/src/ElementQueries';
+import SizeSwapper from '.';
 
 describe('SizeSwapper', () => {
-  it('your test here', () => {});
+  it('renders', () => {
+    const { container } = render(
+      <SizeSwapper items={[<div className="element1" />, <div className="element2" />]} />,
+    );
+    expect(container).toMatchSnapshot();
+    act(() => {
+      // Change the viewport to 500px.
+      window.innerWidth = 500;
+      window.innerHeight = 500;
+    });
+    fireEvent(window, new Event('resize'));
+
+    expect(
+      render(<SizeSwapper items={[<div className="element1" />, <div className="element2" />]} />),
+    ).toMatchSnapshot();
+  });
 });
