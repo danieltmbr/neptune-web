8e5a83f5232e8c445ec2c7e00e48ed107bdd5b42
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-10-21 22:49:21 +0100

    docs: add documentation for decision

--- packages/docs/liveEditorCode/Decision.code.js
+++ packages/docs/liveEditorCode/Decision.code.js
@@ -1,1 +1,24 @@
-<Decision/>;
+() => (
+  <Decision
+    type="NAVIGATION"
+    presentation={Decision.Presentation.LIST_BLOCK}
+    options={[
+      {
+        media: {
+          list: (
+            <Avatar size="md" theme="light" type="initials" className="text-xs-center">
+              HM
+            </Avatar>
+          ),
+          block: (
+            <img src="https://transferwise.com/public-resources/assets/bank-details/bank-details-flow/finish.svg" />
+          ),
+        },
+        title: 'Cat jumps and falls',
+        content:
+          'Cat jumps and falls onto the couch purrs and wakes up in a new dimension filled with kitty litter',
+        onClick: console.log('clicked'),
+      },
+    ]}
+  />
+);
