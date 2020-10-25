6144dcefe947dd589b62485ff70e507fbda8eb09
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-10-25 19:28:39 +0000

    chore: add chainable custom prop type

--- packages/components/src/common/customProps/elementOfType/elementOfType.js
+++ packages/components/src/common/customProps/elementOfType/elementOfType.js
@@ -1,20 +1,48 @@
 import { isString } from '@transferwise/neptune-validation';
 
-export const elementOfType = (allowedType) => (props, propName, componentName) => {
-  if (props[propName]) {
-    const { type } = props[propName];
+const elementOfTypeChecker = (allowedType) => (props, propName, componentName) => {
+  const prop = props[propName];
+
+  if (prop && allowedType) {
+    const { type } = prop;
 
     if (!type) {
-      return null;
+      return errorReturn({ propName, propToCheck: prop, componentName, allowedType });
     }
     const propToCheck = type.displayName || type;
+
     if (isString(propToCheck)) {
       if (allowedType.indexOf(propToCheck) === -1) {
-        return new Error(
-          `Invalid prop ${propName} of type ${propToCheck} provided to ${componentName}. Only ${allowedType} allowed`,
-        );
+        return errorReturn({ propName, propToCheck, componentName, allowedType });
       }
     }
   }
+  // assume all ok
   return null;
 };
+
+const errorReturn = ({ propName, propToCheck, componentName, allowedType }) =>
+  new Error(
+    `Invalid prop ${propName} of type ${propToCheck} provided to ${componentName}. Only ${allowedType} allowed`,
+  );
+
+const createChainableTypeChecker = (validate) => (allowedType) => {
+  const checkType = (isRequired, props, propName, componentName, location) => {
+    const prop = props[propName];
+
+    if (prop == null) {
+      if (isRequired) {
+        return new Error(`Required ${propName} was not specified in ${componentName}.`);
+      }
+      return null;
+    }
+    return validate(allowedType)(props, propName, componentName, location);
+  };
+
+  const chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+};
+
+export const elementOfType = createChainableTypeChecker(elementOfTypeChecker);
